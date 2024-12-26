691e4fe2e30785edde6fcf28d1e7f64a6361874e
Ranish-Garg <ranishgarg2006@gmail.com>
2024-12-26 22:04:44 +0530

    second

--- project.js
+++ project.js
@@ -1,12 +1,3 @@
-let card = document.querySelector(".card");
-
-document.querySelector(".plus").addEventListener("click", () => {
-  card.style.transform = "rotateY(180deg)";
-});
-
-document.querySelector(".minus").addEventListener("click", () => {
-  card.style.transform = "";
-});
 document.getElementById("signup").addEventListener("click", (event) => {
   event.preventDefault();
   window.location.href = "signup.html";
@@ -16,6 +7,172 @@ document.getElementById("signin").addEventListener("click", (event) => {
   event.preventDefault();
   window.location.href = "signin.html";
 });
+
+
+let array=[
+  {
+    image:  "url(images/image1.jpg)",
+    title: "Hyped Monkey #234",
+    price: 56,
+    description:  `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+    tenetur ab velit in et, delectus temporibus, provident
+    perferendis, autem recusandae cumque sequi magni vero nobis
+    eligendi iste incidunt architecto corporis tempora amet? Minima,
+    esse?` 
+  },
+  {
+    image:  "url(images/image2.jpg)",
+    title: "Hyped Monkey #235",
+    price: 102,
+    description:  `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+    tenetur ab velit in et, delectus temporibus, provident
+    perferendis, autem recusandae cumque sequi magni vero nobis
+    eligendi iste incidunt architecto corporis tempora amet? Minima,
+    esse?`
+  },
+  {
+    image:  "url(images/image3.jpg)",
+    title: "Hyped Monkey #236",
+    price: 34,
+    description:  `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+    tenetur ab velit in et, delectus temporibus, provident
+    perferendis, autem recusandae cumque sequi magni vero nobis
+    eligendi iste incidunt architecto corporis tempora amet? Minima,
+    esse?`
+  },
+  {
+    image:  "url(images/image4.jpg)",
+    title: "Hyped Monkey #237",
+    price: 117,
+    description:  `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+    tenetur ab velit in et, delectus temporibus, provident
+    perferendis, autem recusandae cumque sequi magni vero nobis
+    eligendi iste incidunt architecto corporis tempora amet? Minima,
+    esse?`
+  },
+  {
+    image:  "url(images/image5.jpg)",
+    title: "Hyped Monkey #238",
+    price: 78,
+    description:  `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+    tenetur ab velit in et, delectus temporibus, provident
+    perferendis, autem recusandae cumque sequi magni vero nobis
+    eligendi iste incidunt architecto corporis tempora amet? Minima,
+    esse?`
+  },
+  {
+    image:  "url(images/image6.jpg)",
+    title: "Hyped Monkey #239",
+    price: 58,
+    description:  `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+    tenetur ab velit in et, delectus temporibus, provident
+    perferendis, autem recusandae cumque sequi magni vero nobis
+    eligendi iste incidunt architecto corporis tempora amet? Minima,
+    esse?`
+  },
+  {
+    image:  "url(images/image7.jpg)",
+    title: "Hyped Monkey #240",
+    price: 98,
+    description:  `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+    tenetur ab velit in et, delectus temporibus, provident
+    perferendis, autem recusandae cumque sequi magni vero nobis
+    eligendi iste incidunt architecto corporis tempora amet? Minima,
+    esse?`
+  },
+  {
+    image:  "url(images/image8.jpg)",
+    title: "Hyped Monkey #241",
+    price: 68,
+    description:  `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+    tenetur ab velit in et, delectus temporibus, provident
+    perferendis, autem recusandae cumque sequi magni vero nobis
+    eligendi iste incidunt architecto corporis tempora amet? Minima,
+    esse?`
+  },
+  {
+    image:  "url(images/image9.jpg)",
+    title: "Hyped Monkey #242",
+    price: 113,
+    description:  `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+    tenetur ab velit in et, delectus temporibus, provident
+    perferendis, autem recusandae cumque sequi magni vero nobis
+    eligendi iste incidunt architecto corporis tempora amet? Minima,
+    esse?`
+  },
+  
+];
+function arraylength()
+{
+  return array.length;
+}
+function displaycards()
+{
+
+  array.forEach((element) => {
+  
+    let div=document.createElement("div");
+    div.className="card";
+    div.innerHTML=` <div class="front">
+              <div
+                class="image"
+                style="background-image: ${element.image}"
+              ></div>
+              <div class="content">
+                <div class="title">${element.title}</div>
+                <div class="pab">
+                  <div class="price">
+                    Price :
+                    <div class="pvalue">${element.price}</div>
+                  </div>
+                  <button class="buy">Buy</button>
+                </div>
+                <div class="plus" id="flip">+</div>
+              </div>
+            </div>
+            <div class="back">
+              <div class="title">Description :</div>
+              <div class="des">
+                ${element.description}
+              </div>
+              <div class="minus" id="flip">-</div>
+            </div>`
+  
+            document.querySelector(".cards").prepend(div)
+  
+  });
+}
+displaycards();
+
+let card = document.querySelector(".card");
+let cards=document.querySelector(".cards");
+
+// document.querySelector(".plus").addEventListener("click", () => {
+//   card.style.transform = "rotateY(180deg)";
+// });
+
+// document.querySelector(".minus").addEventListener("click", () => {
+//   card.style.transform = "";
+// });
+
+cards.addEventListener("click",(event)=>
+{
+  // event.preventDefault;
+  let target=event.target;
+  console.log(target)
+  let closestcard=target.closest(".card")
+
+  if(target.className=="plus")
+  {
+    closestcard.style.transform = "rotateY(180deg)";
+  }
+  else if(target.className=="minus")
+  {
+    closestcard.style.transform = "";
+  }
+
+})
+
 let sort = document.querySelector(".sort");
 
 let sorthead = document.querySelector(".sorthead");
@@ -46,6 +203,59 @@ sorthead.addEventListener("click", (event) => {
              <br>
             <div class="contentcenter"><button class="sortbtn">Sort</button></div>`
     sort.appendChild(div);
+    let sortbtn=document.querySelector(".sortbtn");
+sortbtn.addEventListener("click",()=>{
+
+  let radio=document.getElementsByName("rad");
+ 
+  if(radio[0].checked)
+  {
+    console.log("hello")
+    let len=arraylength();
+
+    for(let i=0;i<len;i++)
+      {
+        for(let j=i;j<len;j++)
+        {
+            if(array[j].price<array[i].price)
+            {
+              let temp=array[i];
+              array[i]=array[j];
+              array[j]=temp;
+              
+            }
+        }
+      }
+      cards.innerHTML="";
+      displaycards();
+    
+  }
+  else if(radio[1].checked)
+  {
+    let len=arraylength();
+    
+    for(let i=0;i<len;i++)
+    {
+      for(let j=i;j<len;j++)
+      {
+          if(array[j].price>array[i].price)
+          {
+            let temp=array[i];
+            array[i]=array[j];
+            array[j]=temp;
+            
+          }
+      }
+    }
+    cards.innerHTML="";
+    displaycards();
+  }
+  else{
+    alert("Select any radio button!!")
+  }
+  
+})
+
   }
 });
 
@@ -80,3 +290,9 @@ filterhead.addEventListener("click", (event) => {
     filter.appendChild(div);
   }
 });
+
+
+
+
+
+
